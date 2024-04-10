c3f029ae077739afee0c01158e9236866ceda8d2
iamrubynzzz <2018rubinathapa@gmail.com>
2024-04-10 21:12:27 +0545

    retrieve all details of admin through it's id

--- back-end/src/main/java/com/techtitans/backend/controller/AdminController.java
+++ back-end/src/main/java/com/techtitans/backend/controller/AdminController.java
@@ -10,10 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -31,6 +28,15 @@ public class AdminController {
             @RequestBody AdminRequestDto adminRequestDto) {
         AdminResponseDto savedAdmin = adminService.createAdmin(adminRequestDto);
         return new ResponseEntity<>(savedAdmin, HttpStatus.OK);
+    }
+
+    // Build get admin from id from API
+    @GetMapping(PathConstants.GET_BY_ID_PATH)
+    public ResponseEntity<AdminResponseDto> getAdminById(
+            @PathVariable int id
+    ){
+        AdminResponseDto admin = adminService.getAdminById(id);
+        return new ResponseEntity<>(admin, HttpStatus.OK);
     }
 
 }
