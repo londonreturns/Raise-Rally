6af36eb0a9d6c7233a56f09c4fd77fbbec1efe9e
londonreturns <subedioscar.47@gmail.com>
2024-05-08 21:58:31 +0545

    get contributions by backer id

--- back-end/src/main/java/com/techtitans/backend/repository/ContributionRepository.java
+++ back-end/src/main/java/com/techtitans/backend/repository/ContributionRepository.java
@@ -4,6 +4,9 @@ import com.techtitans.backend.entity.ContributionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContributionRepository extends JpaRepository<ContributionEntity, Integer> {
+    List<ContributionEntity> findContributionEntitiesByBackerBackerId(int backerId);
 }
