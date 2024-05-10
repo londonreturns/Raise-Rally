eb2970b4314b25164b5702d055c8215059deb0b4
yamaitech <eishworacharya@gmail.com>
2024-05-10 21:21:38 +0545

    image post completed

--- front-end/src/components/Addimage.jsx
+++ front-end/src/components/Addimage.jsx
@@ -1,7 +1,6 @@
-
 import React, { useState } from 'react';
 import postAxios from '../hooks/postAxios';
-
+import { FaFileUpload } from "react-icons/fa";
 function Addimage() {
   const { makeRequest, isLoading, error } = postAxios("http://localhost:8080/api/images/1");
 
@@ -48,24 +47,94 @@ function Addimage() {
 
     <>
     <div className='text-center p-4 fs-5 fw-medium '>
-        Choose Product Photos
+      Choose Product Photos
+    </div>
+    <div className='d-flex flex-column flex-lg-row  justify-content-center align-items-center  gap-2 p-5 '>
+    <div className='pt-5 Addimage text-center '>
+      <div><FaFileUpload size={40} /></div>
+      <div className='d-flex justify-content-center'>
+        <label  className='btn btn bg-success text-white m-2'>
+          Choose File
+          <input id="file" type="file" accept=".jpg"onChange={(event) => handleImageChange(event, 0)} style={{ display: 'none' }} />
+        </label>
       </div>
       <div>
-        <input type="file" accept=".jpg, .jpeg, .png" onChange={(event) => handleImageChange(event, 0)} />
-        {selectedImages[0] && <img src={URL.createObjectURL(selectedImages[0])} alt="Preview" />}
+        {selectedImages[0] && <img src={URL.createObjectURL(selectedImages[0])} width="150" height="150" alt="Preview" />}
+      </div>
+    </div>
+    <div className='pt-5 Addimage text-center '>
+      <div><FaFileUpload size={40} /></div>
+      <div className='d-flex justify-content-center'>
+        <label className='btn btn bg-success text-white m-2'>
+          Choose File
+          <input id="file" type="file" accept=".jpg"onChange={(event) => handleImageChange(event, 1)} style={{ display: 'none' }} />
+        </label>
       </div>
       <div>
-        <input type="file" accept=".jpg, .jpeg, .png" onChange={(event) => handleImageChange(event, 1)} />
-        {selectedImages[1] && <img src={URL.createObjectURL(selectedImages[1])} alt="Preview" />}
+        {selectedImages[1] && <img src={URL.createObjectURL(selectedImages[1])} width="150" height="150" alt="Preview" />}
+      </div>
+    </div>
+    <div className='pt-5 Addimage text-center '>
+      <div><FaFileUpload size={40} /></div>
+      <div className='d-flex justify-content-center'>
+        <label  className='btn btn bg-success text-white m-2'>
+          Choose File
+          <input id="file" type="file" accept=".jpg"onChange={(event) => handleImageChange(event, 2)} style={{ display: 'none' }} />
+        </label>
       </div>
       <div>
-        <input type="file" accept=".jpg, .jpeg, .png" onChange={(event) => handleImageChange(event, 2)} />
-        {selectedImages[2] && <img src={URL.createObjectURL(selectedImages[2])} alt="Preview" />}
+        {selectedImages[2] && <img src={URL.createObjectURL(selectedImages[2])} width="150" height="150" alt="Preview" />}
       </div>
-      <button onClick={handleUpload} disabled={isLoading}>Upload Images</button>
-      {isLoading && <p>Uploading...</p>}
-      {error && <p>Error: {error.message}</p>}
-    </>
+      <div className=' d-inline-block btn btn-primary position-absolute mt-5 ms-5' data-bs-toggle="modal"
+                  data-bs-target="#exampleModal">
+                Continue
+            </div>
+            <div
+          className="modal fade "
+          id="exampleModal"
+          tabIndex={-1}
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header bg-dark-subtle">
+                <h1 className="modal-title fs-4" id="exampleModalLabel">
+                  Confirm AddProduct
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                />
+              </div>
+              <div className="modal-footer ">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Cancel
+                </button>
+                <button onClick={handleUpload}
+                  type="button"
+                  className="btn btn-success"
+                 
+                >
+                  Add
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+                </div>
+                
+                
+                
+             </div>
+</>
   );
 }
 
