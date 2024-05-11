7eb90243603834c69df529f57bcd43fa0fda7138
londonreturns <subedioscar.47@gmail.com>
2024-05-11 14:05:48 +0545

    price added in benefit dto

--- back-end/src/main/java/com/techtitans/backend/dto/benefit/BenefitResponseDto.java
+++ back-end/src/main/java/com/techtitans/backend/dto/benefit/BenefitResponseDto.java
@@ -1,5 +1,6 @@
 package com.techtitans.backend.dto.benefit;
 
+import com.techtitans.backend.dto.price.PriceResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,7 @@ public class BenefitResponseDto implements Serializable {
     private int benefitId;
     private String benefitName;
     private String benefitDescription;
-    private int priceId;
+    private PriceResponseDto price;
     private int productId;
     private List<Integer> contributionIds;
 }
