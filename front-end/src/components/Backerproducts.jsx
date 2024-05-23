c06779c1001596e928b4fca59bdb5f5b73f0c8ba
yamaitech <eishworacharya@gmail.com>
2024-05-24 00:36:28 +0545

    card for funded project

---
+++ front-end/src/components/Backerproducts.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+//product photo
+//productName
+//benefit Name 
+//goal amount
+//paid amount
+//payment date
+function Backerproducts() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Backerproducts
--- front-end/src/components/Trial.jsx
+++ front-end/src/components/Trial.jsx
@@ -1,32 +1,62 @@
-import React, { useEffect } from 'react';
-import postAxios from '../hooks/postAxios';
+import React from 'react'
+import { useEffect} from 'react';
+import getAxios from '../hooks/getAxios';
+import useImageConverter from "../hooks/imageConverter";
+//product photo
+//productName
+//benefit Name 
+//goal amount
+//paid amount
+//payment date
+function Trial(
+  {
+    paymentDate,
+    actualPaidPrice,
+    benefitId,
+    backerId
+  }
+) {
 
-function Trial() {
-  const { makeRequest, data } = postAxios("http://localhost:8080/api/payment/savePayment");
-  const payload = {
-    actualPaidPrice: 111,
-    backerId: 25,
-    benefitId: 25,
-  };
 
-  const post = () => {
-    makeRequest(payload);
-  };
+ 
+  const {data:fundedprojects}=getAxios("http://localhost:8080/api/products/fundedproducts/7");
+  const { data: benefit } = getAxios(
+    `http://localhost:8080/api/benefits/${benefitId}`
+  );
+  const { convertedFile, convertImage } = useImageConverter();
 
-  // Use useEffect to log data when it changes
   useEffect(() => {
-    if (data !== null) {
-      console.log(data);
-    }
-  }, [data]);
+    // URL of the image to convert
+    const imageUrl = `http://localhost:8080/api/images/${1}_1.jpeg`;
+    // Call the convertImage function when the component mounts
+    convertImage(imageUrl);
+  }, [convertImage]); // Run this effect only once after initial render
 
   return (
     <>
-      <div className="btn btn-success" onClick={post}>
-        Submit Payment
+
+<div className=" col-lg-3 bg-success-subtle rounded ">
+      {convertedFile && (
+                  <img 
+                  src={URL.createObjectURL(convertedFile)}
+                 
+                  className=" card-img-top pt-3 rounded" 
+                  alt="..."
+                  style={{width:"200px",height:"200px"}}
+              />
+                )}
+      
+      <div >
+        <p ><strong>Benefit Name:</strong> {benefit.benefitName}</p>
+        {/* <p className="card-text"><strong>Goal Amount:</strong> {fundedproject.productGoal}</p> */}
+        <p ><strong>Paid Amount:</strong> {actualPaidPrice}</p>
+        <p ><strong>Payment Date:</strong> {paymentDate}</p>
       </div>
+    </div>
+
+   
     </>
-  );
+  )
 }
 
-export default Trial;
+export default Trial
